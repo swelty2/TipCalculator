4625c464f1ff92189e80ff35c6da8cd4d2db434a
swelty2 <swelty2@uncc.edu>
2019-09-11 14:22:56 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Haley on 9/11/19.
+//  Copyright © 2019 Sarah Welty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
