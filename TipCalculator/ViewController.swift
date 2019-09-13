d3c3c64f828d67efbaf49a83f30751d053406274
swelty2 <swelty2@uncc.edu>
2019-09-13 17:56:49 -0400

    Added layout constraints

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TipCalculator
 //
-//  Created by Haley on 9/11/19.
+//  Created by Sarah on 9/11/19.
 //  Copyright © 2019 Sarah Welty. All rights reserved.
 //
 
@@ -10,49 +10,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-   
-    
-    @IBOutlet weak var currentValueLabel: UILabel!
-    
     @IBOutlet weak var billValue: UITextField!
     
+    @IBOutlet weak var tipSegmentValue: UISegmentedControl!
     
     
+    @IBOutlet weak var customValue: UISlider!
+    
+  
+    @IBOutlet weak var billTotal: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Checking user input \n line 26")
-        //Check that bill total is positive
-        let tempBillVal = self.billValue
-        print("User input- Bill Value =\(tempBillVal)")
-        
-        if (tempBillVal < 0.0) {
-            
-        } else {
-            
-        }//end of else
-        
-        
+
     }//end of viewDidLoad()
     
     //------------------------ Class Methods
     
-    //Segmented Control Logic
-    @IBAction func segmentValChanged(_ sender: UISegmentedControl) {
-        print("segmentValueChanged\(sender.selectedSegmentIndex)")
-         // segmentedControl.addTarget(self, action: "action:", forControlEvents: .ValueChanged)
-    }
     
-    
-    //Slider Control Logic
-    
-    @IBAction func onSliderValChanged(_ sender: UISlider) {
-        print("current value is \(sender.value)")
-        self.currentValueLabel.text = String(sender.value) + "%"
+
+    @IBAction func tipSelectedSegement(_ sender: UISegmentedControl) {
+        
+        let bill = Double(billValue.text!)
+        
+        if ((bill == nil) || (bill! <= 0)) {
+            print("Error Handler- Bill total is NIL or NEGATIVE")
+            
+            //alert user that first name field is empty
+            let alert = UIAlertController(title: "Error", message: "Bill total must be a positive value (example: 25.67)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }//end of if
+        
+        else {
+            //now calculate the tip based on segment chosen
+            if (self.tipSegmentValue.selectedSegmentIndex == 0 ) {
+                //tip is 10%
+                let tipPerc = 0.10
+                
+                
+            } else if (self.tipSegmentValue.selectedSegmentIndex == 1) {
+                 let tipPerc = 0.15
+                
+            } else if (self.tipSegmentValue.selectedSegmentIndex == 2 ) {
+                 let tipPerc = 0.18
+                
+            } else if (self.tipSegmentValue.selectedSegmentIndex == 3) {
+                
+            }
+            
+            
+        }
         
     }
+    
+  
+    /*
+    @IBAction func billTotalValueChanged(_ sender: UITextField) {
+        //Check that bill total is positive & not nil
+        
+        //unwrap bill total user input field
+        let billTotal = Double(billValue.text!)
+        
+        // Check that input is numerical and is a positive val
+        if (billTotal! <= 0) {
+            //alert user that bill value is not a positive value
+            let alert = UIAlertController(title: "Error", message: "Bill Total Must Be A Positive Number", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }//end of if
+        
+        if (billTotal == nil) {
+            //alert user that first name field is empty
+            let alert = UIAlertController(title: "Error", message: "Input Bill Total", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
+        print("User successfully entered Bill Total")
+        
+    }
+    
+    */
+
+    
+    //Slider Control Logic
+  
     
     
     
