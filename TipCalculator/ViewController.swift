89d996940bc1d437ffde8e84d9abfea3fecb4f2b
swelty2 <swelty2@uncc.edu>
2019-09-13 15:25:09 -0400

    App setup

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var currentValueLabel: UILabel!
+    
+  
 
-
+    //Segmented Control Logic
+    @IBAction func segmentValChanged(_ sender: UISegmentedControl) {
+        print("segmentValueChanged\(sender.selectedSegmentIndex)")
+         // segmentedControl.addTarget(self, action: "action:", forControlEvents: .ValueChanged)
+    }
+    
+    //Slider Control Logic
+    
+    @IBAction func onSliderValChanged(_ sender: UISlider) {
+        print("current value is \(sender.value)")
+        self.currentValueLabel.text = String(sender.value) + "%"
+        
+    }
+    
+    
+    
 }
 
