c7fcadea61e4a8f1d47609f9b7effd45683f7e4c
swelty2 <swelty2@uncc.edu>
2019-09-13 15:58:15 -0400

    Interface built
    Text areas, labels, and buttons on MAIN VIEW created

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,20 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
+   
     
     @IBOutlet weak var currentValueLabel: UILabel!
     
-  
-
+    @IBOutlet weak var billValue: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Checking user input \n line 26")
+        //Check that bill total is positive
+        let tempBillVal = self.billValue
+        print("User input- Bill Value =\(tempBillVal)")
+        
+        if (tempBillVal < 0.0) {
+            
+        } else {
+            
+        }//end of else
+        
+        
+    }//end of viewDidLoad()
+    
+    //------------------------ Class Methods
+    
     //Segmented Control Logic
     @IBAction func segmentValChanged(_ sender: UISegmentedControl) {
         print("segmentValueChanged\(sender.selectedSegmentIndex)")
          // segmentedControl.addTarget(self, action: "action:", forControlEvents: .ValueChanged)
     }
+    
     
     //Slider Control Logic
     
